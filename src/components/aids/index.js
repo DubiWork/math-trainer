b8c985dc35626c159b1eb32d588e73bb39b503d3
DubiWork <dubigwork@gmail.com>
2026-03-09 19:08:16 +0200

    feat(#65): add NumberLine SVG component for Tier 1 visual aids

--- src/components/aids/index.js
+++ src/components/aids/index.js
@@ -3,6 +3,8 @@
  *
  * Tier 1 aids help children visualise math operations:
  * - DotCounter: Coloured dot groups for addition and subtraction
+ * - NumberLine: SVG number line with animated jump arcs
  */
 
 export { default as DotCounter } from './DotCounter'
+export { default as NumberLine } from './NumberLine'
